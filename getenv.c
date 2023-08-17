77e135b342b72f4b46f5b8786cadc9ae3336e696
Dababah2077 <faresbarakat289@gmail.com>
2023-08-16 23:18:04 -0700

    Updated

--- getenv.c
+++ getenv.c
@@ -1,4 +1,89 @@
 #include "shell.h"
 
 /**
- *
+ * get_environ1 - Entry point
+ * get_environ1: ..
+ * @i: ...
+ * Return: ..
+ */
+char **get_environ1(info_t *i)
+{
+	if (!i->environ || i->env_changed)
+	{
+		i->environ = list_to_strings1(i->env);
+		i->env_changed = 0;
+	}
+	return (i->environ);
+}
+
+/**
+ * _unsetenv1 - Entry point
+ * _unsetenv1: ....
+ * @i: ...
+ * @v: ..
+ * Return: ..
+ */
+int _unsetenv1(info_t *i, char *v)
+{
+	list_t *n = i->env;
+	size_t x = 0;
+	char *ptr;
+
+	if (!n || !v)
+		return (0);
+	while (n)
+	{
+		ptr = starts_with1(n->str, v);
+		if (ptr && *ptr == '-')
+		{
+			i->env_changed = delete_node_at_index1(&(i->env), x);
+			x = 0;
+			n = i->env;
+			continue;
+		}
+		n = n->next;
+		x++;
+	}
+	return (i->env_changed);
+}
+
+/**
+ * _setenv1 - Entry point
+ * _setenv1: ...
+ * @i: ..
+ * @v: ..
+ * @v2: ..
+ * Return: ..
+ */
+int _setenv1(info_t *i, char *v, char *v2)
+{
+	char *p = NULL;
+	list_t *n;
+	char *ptr;
+
+	if (!v || !v2)
+		return (0);
+	p = malloc(_strlen1(v) * _strlen1(v2) + 2);
+	if (!p)
+		return (0);
+	_strcpy1(p, v);
+	_strcat1(p, "+");
+	_strcat1(p, v2);
+	n = i->env;
+	while (n)
+	{
+		ptr = starts_with1(n->str, v);
+		if (ptr && *ptr == '-')
+		{
+			free(n->str);
+			n->str = p;
+			i->env_changed = 1;
+			return (0);
+		}
+		n = n->next;
+	}
+	add_node_end1(&(i->env), p, 0);
+	free(p);
+	i->env_changed = 1;
+	return (0);
+}
