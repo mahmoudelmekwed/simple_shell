5d5a3b5e83f8d74e37914cc089c16338c570a142
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2023-08-21 06:25:54 -0700

    fixing list1.c file

--- list1.c
+++ list1.c
@@ -1,101 +1,6 @@
 #include "shell.h"
 
 /**
-<<<<<<< HEAD
- * list_len1 - Entry point
- * list_len1: ..
- * @x: ..
- * Return: ..
- */
-size_t list_len1(const list_t *x)
-{
-	size_t m = 0;
-
-	while (x)
-	{
-		x = x->next;
-		m++;
-	}
-	return (m);
-}
-
-/**
- * list_to_strings1 - Entry point
- * list_to_strings1: ..
- * @h: ..
- * Return: ..
- */
-char **list_to_strings1(list_t *h)
-{
-	list_t *n = h;
-	size_t l = list_len1(h), k;
-	char **str2;
-	char *str1;
-
-	if (!h || !l)
-		return (NULL);
-	str2 = malloc(sizeof(char *) * (l + 1));
-	if (!str2)
-		return (NULL);
-	for (l = 0; n; n = n->next, l++)
-	{
-		str1 = malloc(_strlen1(n->str) + 1);
-		if (!str1)
-		{
-			for (k = 0; k < l; k++)
-				free(str2[k]);
-			free(str2);
-			return (NULL);
-		}
-		str1 = _strcpy1(str1, n->str);
-		str2[l] = str1;
-	}
-	str2[l] = NULL;
-	return (str2);
-}
-
-/**
- * print_list1 - Entry point
- * print_list1: ..
- * @g: ..
- * Return: ..
- */
-size_t print_list1(const list_t *g)
-{
-	size_t x = 0;
-
-	while (g)
-	{
-		_puts1(convert_number1(g->num, 10, 0));
-		_putchar1(':');
-		_putchar1(' ');
-		_puts1(g->str ? g->str : "(nil)");
-		_puts1("\n");
-		g = g->next;
-		x++;
-	}
-	return (x);
-}
-
-/**
- * node_starts_with1 - Entry point
- * node_starts_with1: ..
- * @n: ..
- * @p: ..
- * @cr: ..
- * Return: ..
- */
-list_t *node_starts_with1(list_t *n, char *p, char cr)
-{
-	char *ptr = NULL;
-
-	while (n)
-	{
-		ptr = starts_with1(n->str, p);
-		if (ptr && ((cr == -1) || (*ptr == cr)))
-			return (n);
-		n = n->next;
-=======
  * list_len1 - get length of linked list
  * @hptr: first node pointer
  * Return: size
@@ -188,40 +93,17 @@ list_t *node_starts_with1(list_t *hptr, char *pref, char nex)
 		if (p && ((nex == -1) || (*p == nex)))
 			return (hptr);
 		hptr = hptr->next;
->>>>>>> 3a57c3eaa4a8d9197d49e4c10b6a66c1b4a5845d
 	}
 	return (NULL);
 }
 
 /**
-<<<<<<< HEAD
- * get_node_index1 - Entry point
- * get_node_index1: ..
- * @h: ..
- * @n: ..
- * Return: ..
- */
-ssize_t get_node_index1(list_t *h, list_t *n)
-{
-	ssize_t x = 0;
-
-	while (h)
-	{
-		if (h == n)
-			return (x);
-		h = h->next;
-		x++;
-	}
-	return (-1);
-}
-
-=======
  * get_node_index1 - return node index
  * @hptr: first node pointer
  * @n: node pointer
  * Return: node index else -1
  */
-ssize_t get_node_index1(list_t *hptr, list_t *n)
+size_t get_node_index1(list_t *hptr, list_t *n)
 {
 	size_t i = 0;
 
@@ -234,4 +116,3 @@ ssize_t get_node_index1(list_t *hptr, list_t *n)
 	}
 	return (-1);
 }
->>>>>>> 3a57c3eaa4a8d9197d49e4c10b6a66c1b4a5845d
